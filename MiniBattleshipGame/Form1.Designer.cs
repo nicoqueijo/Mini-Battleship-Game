63069aa18f282adc280f0a47628693c344bea25c
Nicolas <nicoqueijo@hotmail.com>
2017-06-15 15:43:10 -0400

    first commit

---
+++ MiniBattleshipGame/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace MiniBattleshipGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.board_label_0_0 = new System.Windows.Forms.Label();
+            this.board_label_0_3 = new System.Windows.Forms.Label();
+            this.board_label_0_2 = new System.Windows.Forms.Label();
+            this.board_label_0_1 = new System.Windows.Forms.Label();
+            this.board_label_1_3 = new System.Windows.Forms.Label();
+            this.board_label_1_2 = new System.Windows.Forms.Label();
+            this.board_label_1_1 = new System.Windows.Forms.Label();
+            this.board_label_1_0 = new System.Windows.Forms.Label();
+            this.board_label_2_2 = new System.Windows.Forms.Label();
+            this.board_label_2_3 = new System.Windows.Forms.Label();
+            this.board_label_2_1 = new System.Windows.Forms.Label();
+            this.board_label_2_0 = new System.Windows.Forms.Label();
+            this.board_label_3_3 = new System.Windows.Forms.Label();
+            this.board_label_3_2 = new System.Windows.Forms.Label();
+            this.board_label_3_1 = new System.Windows.Forms.Label();
+            this.board_label_3_0 = new System.Windows.Forms.Label();
+            this.start_button = new System.Windows.Forms.Button();
+            this.game_status_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // board_label_0_0
+            // 
+            this.board_label_0_0.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_0_0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_0_0.Location = new System.Drawing.Point(86, 15);
+            this.board_label_0_0.Name = "board_label_0_0";
+            this.board_label_0_0.Size = new System.Drawing.Size(75, 75);
+            this.board_label_0_0.TabIndex = 0;
+            this.board_label_0_0.Click += new System.EventHandler(this.board_label_0_0_Click);
+            // 
+            // board_label_0_3
+            // 
+            this.board_label_0_3.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_0_3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_0_3.Location = new System.Drawing.Point(329, 15);
+            this.board_label_0_3.Name = "board_label_0_3";
+            this.board_label_0_3.Size = new System.Drawing.Size(75, 75);
+            this.board_label_0_3.TabIndex = 1;
+            this.board_label_0_3.Click += new System.EventHandler(this.board_label_0_3_Click);
+            // 
+            // board_label_0_2
+            // 
+            this.board_label_0_2.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_0_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_0_2.Location = new System.Drawing.Point(248, 15);
+            this.board_label_0_2.Name = "board_label_0_2";
+            this.board_label_0_2.Size = new System.Drawing.Size(75, 75);
+            this.board_label_0_2.TabIndex = 2;
+            this.board_label_0_2.Click += new System.EventHandler(this.board_label_0_2_Click);
+            // 
+            // board_label_0_1
+            // 
+            this.board_label_0_1.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_0_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_0_1.Location = new System.Drawing.Point(167, 15);
+            this.board_label_0_1.Name = "board_label_0_1";
+            this.board_label_0_1.Size = new System.Drawing.Size(75, 75);
+            this.board_label_0_1.TabIndex = 3;
+            this.board_label_0_1.Click += new System.EventHandler(this.board_label_0_1_Click);
+            // 
+            // board_label_1_3
+            // 
+            this.board_label_1_3.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_1_3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_1_3.Location = new System.Drawing.Point(329, 99);
+            this.board_label_1_3.Name = "board_label_1_3";
+            this.board_label_1_3.Size = new System.Drawing.Size(75, 75);
+            this.board_label_1_3.TabIndex = 4;
+            this.board_label_1_3.Click += new System.EventHandler(this.board_label_1_3_Click);
+            // 
+            // board_label_1_2
+            // 
+            this.board_label_1_2.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_1_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_1_2.Location = new System.Drawing.Point(248, 99);
+            this.board_label_1_2.Name = "board_label_1_2";
+            this.board_label_1_2.Size = new System.Drawing.Size(75, 75);
+            this.board_label_1_2.TabIndex = 5;
+            this.board_label_1_2.Click += new System.EventHandler(this.board_label_1_2_Click);
+            // 
+            // board_label_1_1
+            // 
+            this.board_label_1_1.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_1_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_1_1.Location = new System.Drawing.Point(167, 99);
+            this.board_label_1_1.Name = "board_label_1_1";
+            this.board_label_1_1.Size = new System.Drawing.Size(75, 75);
+            this.board_label_1_1.TabIndex = 6;
+            this.board_label_1_1.Click += new System.EventHandler(this.board_label_1_1_Click);
+            // 
+            // board_label_1_0
+            // 
+            this.board_label_1_0.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_1_0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_1_0.Location = new System.Drawing.Point(86, 99);
+            this.board_label_1_0.Name = "board_label_1_0";
+            this.board_label_1_0.Size = new System.Drawing.Size(75, 75);
+            this.board_label_1_0.TabIndex = 7;
+            this.board_label_1_0.Click += new System.EventHandler(this.board_label_1_0_Click);
+            // 
+            // board_label_2_2
+            // 
+            this.board_label_2_2.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_2_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_2_2.Location = new System.Drawing.Point(248, 183);
+            this.board_label_2_2.Name = "board_label_2_2";
+            this.board_label_2_2.Size = new System.Drawing.Size(75, 75);
+            this.board_label_2_2.TabIndex = 8;
+            this.board_label_2_2.Click += new System.EventHandler(this.board_label_2_2_Click);
+            // 
+            // board_label_2_3
+            // 
+            this.board_label_2_3.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_2_3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_2_3.Location = new System.Drawing.Point(329, 183);
+            this.board_label_2_3.Name = "board_label_2_3";
+            this.board_label_2_3.Size = new System.Drawing.Size(75, 75);
+            this.board_label_2_3.TabIndex = 9;
+            this.board_label_2_3.Click += new System.EventHandler(this.board_label_2_3_Click);
+            // 
+            // board_label_2_1
+            // 
+            this.board_label_2_1.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_2_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_2_1.Location = new System.Drawing.Point(167, 183);
+            this.board_label_2_1.Name = "board_label_2_1";
+            this.board_label_2_1.Size = new System.Drawing.Size(75, 75);
+            this.board_label_2_1.TabIndex = 10;
+            this.board_label_2_1.Click += new System.EventHandler(this.board_label_2_1_Click);
+            // 
+            // board_label_2_0
+            // 
+            this.board_label_2_0.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_2_0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_2_0.Location = new System.Drawing.Point(86, 183);
+            this.board_label_2_0.Name = "board_label_2_0";
+            this.board_label_2_0.Size = new System.Drawing.Size(75, 75);
+            this.board_label_2_0.TabIndex = 11;
+            this.board_label_2_0.Click += new System.EventHandler(this.board_label_2_0_Click);
+            // 
+            // board_label_3_3
+            // 
+            this.board_label_3_3.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_3_3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_3_3.Location = new System.Drawing.Point(329, 267);
+            this.board_label_3_3.Name = "board_label_3_3";
+            this.board_label_3_3.Size = new System.Drawing.Size(75, 75);
+            this.board_label_3_3.TabIndex = 12;
+            this.board_label_3_3.Click += new System.EventHandler(this.board_label_3_3_Click);
+            // 
+            // board_label_3_2
+            // 
+            this.board_label_3_2.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_3_2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_3_2.Location = new System.Drawing.Point(248, 267);
+            this.board_label_3_2.Name = "board_label_3_2";
+            this.board_label_3_2.Size = new System.Drawing.Size(75, 75);
+            this.board_label_3_2.TabIndex = 13;
+            this.board_label_3_2.Click += new System.EventHandler(this.board_label_3_2_Click);
+            // 
+            // board_label_3_1
+            // 
+            this.board_label_3_1.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_3_1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_3_1.Location = new System.Drawing.Point(167, 267);
+            this.board_label_3_1.Name = "board_label_3_1";
+            this.board_label_3_1.Size = new System.Drawing.Size(75, 75);
+            this.board_label_3_1.TabIndex = 14;
+            this.board_label_3_1.Click += new System.EventHandler(this.board_label_3_1_Click);
+            // 
+            // board_label_3_0
+            // 
+            this.board_label_3_0.BackColor = System.Drawing.Color.DarkGray;
+            this.board_label_3_0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.board_label_3_0.Location = new System.Drawing.Point(86, 267);
+            this.board_label_3_0.Name = "board_label_3_0";
+            this.board_label_3_0.Size = new System.Drawing.Size(75, 75);
+            this.board_label_3_0.TabIndex = 15;
+            this.board_label_3_0.Click += new System.EventHandler(this.board_label_3_0_Click);
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(198, 407);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(88, 29);
+            this.start_button.TabIndex = 16;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // game_status_label
+            // 
+            this.game_status_label.Location = new System.Drawing.Point(167, 364);
+            this.game_status_label.Name = "game_status_label";
+            this.game_status_label.Size = new System.Drawing.Size(156, 26);
+            this.game_status_label.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 453);
+            this.Controls.Add(this.game_status_label);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.board_label_3_0);
+            this.Controls.Add(this.board_label_3_1);
+            this.Controls.Add(this.board_label_3_2);
+            this.Controls.Add(this.board_label_3_3);
+            this.Controls.Add(this.board_label_2_0);
+            this.Controls.Add(this.board_label_2_1);
+            this.Controls.Add(this.board_label_2_3);
+            this.Controls.Add(this.board_label_2_2);
+            this.Controls.Add(this.board_label_1_0);
+            this.Controls.Add(this.board_label_1_1);
+            this.Controls.Add(this.board_label_1_2);
+            this.Controls.Add(this.board_label_1_3);
+            this.Controls.Add(this.board_label_0_1);
+            this.Controls.Add(this.board_label_0_2);
+            this.Controls.Add(this.board_label_0_3);
+            this.Controls.Add(this.board_label_0_0);
+            this.Name = "Form1";
+            this.Text = "Mini Battleship Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label board_label_0_0;
+        private System.Windows.Forms.Label board_label_0_3;
+        private System.Windows.Forms.Label board_label_0_2;
+        private System.Windows.Forms.Label board_label_0_1;
+        private System.Windows.Forms.Label board_label_1_3;
+        private System.Windows.Forms.Label board_label_1_2;
+        private System.Windows.Forms.Label board_label_1_1;
+        private System.Windows.Forms.Label board_label_1_0;
+        private System.Windows.Forms.Label board_label_2_2;
+        private System.Windows.Forms.Label board_label_2_3;
+        private System.Windows.Forms.Label board_label_2_1;
+        private System.Windows.Forms.Label board_label_2_0;
+        private System.Windows.Forms.Label board_label_3_3;
+        private System.Windows.Forms.Label board_label_3_2;
+        private System.Windows.Forms.Label board_label_3_1;
+        private System.Windows.Forms.Label board_label_3_0;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Label game_status_label;
+    }
+}
+
---
+++ MiniBattleshipGame/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniBattleshipGame
+{
+    public partial class Form1 : Form
+    {
+        private const int HITS_TO_SINK_ALL_SHIPS = 4;
+        private const int OFFSET = 1;
+        private const int ROWS = 4;
+        private const int COLS = 4;
+
+        private const int EMPTY = 0;
+        private const int OCC_SHIP_1 = 1;
+        private const int OCC_SHIP_2 = 2;
+
+        private Random rand = new Random();
+        private Label[,] boardLabels;
+        private int[,] boardLabelsStatus;
+        private int moves = 0;
+        private int hits = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            boardLabels = new Label[ROWS, COLS]
+            {
+                {board_label_0_0, board_label_0_1, board_label_0_2, board_label_0_3},
+                {board_label_1_0, board_label_1_1, board_label_1_2, board_label_1_3},
+                {board_label_2_0, board_label_2_1, board_label_2_2, board_label_2_3},
+                {board_label_3_0, board_label_3_1, board_label_3_2, board_label_3_3}
+            };
+
+            boardLabelsStatus = new int[ROWS, COLS];
+            resetBoard();
+        }
+
+        private void placeFirstShip()
+        {
+            int randRow = rand.Next(ROWS);
+            int randCol = rand.Next(COLS - OFFSET);
+            boardLabelsStatus[randRow, randCol] = OCC_SHIP_1;
+            boardLabelsStatus[randRow, randCol + OFFSET] = OCC_SHIP_1;
+        }
+
+        private void placeSecondShip()
+        {
+            int randRow = 0;
+            int randCol = 0;
+            do
+            {
+                randRow = rand.Next(ROWS - OFFSET);
+                randCol = rand.Next(COLS);
+            } while (boardLabelsStatus[randRow, randCol] != EMPTY || boardLabelsStatus[randRow + OFFSET, randCol] != EMPTY);
+            boardLabelsStatus[randRow, randCol] = OCC_SHIP_2;
+            boardLabelsStatus[randRow + OFFSET, randCol] = OCC_SHIP_2;
+        }
+
+        private void handleBoardLabelClick(int row, int col)
+        {
+            if (hits < HITS_TO_SINK_ALL_SHIPS)
+            {
+                if (boardLabels[row, col].BackColor == Color.DarkGray)
+                {
+                    int labelStatus = boardLabelsStatus[row, col];
+                    switch (labelStatus)
+                    {
+                        case EMPTY:
+                            boardLabels[row, col].BackColor = Color.DodgerBlue;
+                            break;
+                        case OCC_SHIP_1:
+                            boardLabels[row, col].BackColor = Color.Maroon;
+                            hits++;
+                            break;
+                        case OCC_SHIP_2:
+                            boardLabels[row, col].BackColor = Color.Firebrick;
+                            hits++;
+                            break;
+                    }
+                    moves++;
+                    if (hits >= HITS_TO_SINK_ALL_SHIPS)
+                    {
+                        game_status_label.Text = "You won in " + moves + " moves!";
+                    }
+                }
+            }
+             
+        }
+
+        private void resetBoard()
+        {
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int col = 0; col < COLS; col++)
+                {
+                    boardLabels[row, col].BackColor = Color.DarkGray;
+                    boardLabelsStatus[row, col] = EMPTY;
+                }
+            }
+            placeFirstShip();
+            placeSecondShip();
+            moves = 0;
+            hits = 0;
+            game_status_label.Text = "";
+        }
+
+        private void board_label_0_0_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(0, 0);
+        }
+
+        private void board_label_0_1_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(0, 1);
+        }
+
+        private void board_label_0_2_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(0, 2);
+        }
+
+        private void board_label_0_3_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(0, 3);
+        }
+
+        private void board_label_1_0_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(1, 0);
+        }
+
+        private void board_label_1_1_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(1, 1);
+        }
+
+        private void board_label_1_2_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(1, 2);
+        }
+
+        private void board_label_1_3_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(1, 3);
+        }
+
+        private void board_label_2_0_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(2, 0);
+        }
+
+        private void board_label_2_1_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(2, 1);
+        }
+
+        private void board_label_2_2_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(2, 2);
+        }
+
+        private void board_label_2_3_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(2, 3);
+        }
+
+        private void board_label_3_0_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(3, 0);
+        }
+
+        private void board_label_3_1_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(3, 1);
+        }
+
+        private void board_label_3_2_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(3, 2);
+        }
+
+        private void board_label_3_3_Click(object sender, EventArgs e)
+        {
+            handleBoardLabelClick(3, 3);
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            resetBoard();
+        }
+    }
+}
